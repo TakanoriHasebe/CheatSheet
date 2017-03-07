8b865aa83c53ca3be2c75887f88795ad22cb35c1
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-03-07 15:12:14 +0900

    ボタンを押すことによるアニメーションの変更の追加

--- Swift/SwiftButton/SwiftButton/NextViewController.swift
+++ Swift/SwiftButton/SwiftButton/NextViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    @IBOutlet var changeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func change(_ sender: Any) {
+        
+        // ここが呼ばれる
+        
+        // 次の画面へ書かれたラベルの文字を変更したい
+        changeLabel.text = "暗号が解除されました。"
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Swift/SwiftButton/SwiftButton/ViewController.swift
+++ Swift/SwiftButton/SwiftButton/ViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 勝手に呼ばれる
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        
     }
 
+    
+    @IBAction func tap(_ sender: Any) {
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
