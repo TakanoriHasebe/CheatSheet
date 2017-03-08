96b27d4609f14470656e3879c43a46c7b534984e
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-03-08 14:55:32 +0900

    SwiftRunnerから

---
+++ Swift/SwiftRunner/SwiftRunner/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  SwiftRunner
+//
+//  Created by Takanori.H on 2017/03/08.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    
+    @IBOutlet var runnerImageView: UIImageView!
+    
+    var imageArray = [UIImage]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    @IBAction func start(_ sender: AnyObject) {
+        
+        
+        
+    }
+    
+    
+    @IBAction func stop(_ sender: AnyObject) {
+        
+        
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        
+    }
+
+    
+    
+
+}
+
