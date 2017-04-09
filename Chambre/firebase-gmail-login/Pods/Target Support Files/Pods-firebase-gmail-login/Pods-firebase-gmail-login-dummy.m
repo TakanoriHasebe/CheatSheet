237f5e05162bb8400aec407ddc23c17466eb10f9
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-04-09 11:10:08 +0900

    04-09 first commit

--- Chambre/firebase-gmail-login/Pods/Target Support Files/Pods-firebase-gmail-login/Pods-firebase-gmail-login-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_firebase_gmail_login : NSObject
-@end
-@implementation PodsDummy_Pods_firebase_gmail_login
-@end
--- Chambre/phone-number-auth-0/Pods/Target Support Files/Pods-phone-number-auth-0/Pods-phone-number-auth-0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_phone_number_auth_0 : NSObject
-@end
-@implementation PodsDummy_Pods_phone_number_auth_0
-@end
