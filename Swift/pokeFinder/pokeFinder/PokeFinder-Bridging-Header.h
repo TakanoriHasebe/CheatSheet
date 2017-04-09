64bb023af64cf92310e524824f4adbd85c1fba8d
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-04-09 17:16:45 +0900

    udemy 137まで追加

---
+++ Swift/pokeFinder/pokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  pokeFinder
+//
+//  Created by Takanori.H on 2017/04/09.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+#import "GeoFire.h"
--- Swift/pokeFinder/pokeFinder/ViewController.swift
+++ Swift/pokeFinder/pokeFinder/ViewController.swift
@@ -7,15 +7,94 @@
 //
 
 import UIKit
+import MapKit // コピペ
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    // コピペ
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        
     
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        
+        locationAuthStatus()
+        
+    }
+    
+    // コピペ
+    // アプリを使っている時にトラッキング
+    func locationAuthStatus(){
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            
+            mapView.showsUserLocation = true
+            
+        } else{
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+    }
+    
+    // コピペ
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse{
+            mapView.showsUserLocation = true
+        }
+        
+    }
+    
+    // コピペ
+    // マップの大きさ
+    func centerMapLocation(location: CLLocation){
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    // コピペ
+    // ユーザー情報を中央に配置
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        if let loc = userLocation.location{
+            
+            if !mapHasCenteredOnce{
+                centerMapLocation(location: loc)
+                mapHasCenteredOnce = true
+            }
+            
+        }
+        
+    }
+    
+    
+    // コピペ
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self){
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        
+        return annotationView
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
@@ -23,6 +102,11 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func spotRandomPokemon(_ sender: AnyObject) {
+        
+        
+        
+    }
 
 }
 
