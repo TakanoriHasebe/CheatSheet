0184f73e76236cc2803660314663c17eee8c412a
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-04-09 11:42:46 +0900

    GeoFire.hまで終了

---
+++ Swift/pokeFinder/pokeFinder/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  pokeFinder
+//
+//  Created by Takanori.H on 2017/04/09.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        
+    }
+
+
+}
+
