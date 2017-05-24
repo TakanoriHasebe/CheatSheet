ace3157659e74e41078862dbfced3a85ce92c65b
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-05-24 17:29:15 +0900

    CPMenuViewについて追加

---
+++ Swift/SwiftPinterestMotion/Pods/Target Support Files/Pods-SwiftPinterestMotion/Pods-SwiftPinterestMotion-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftPinterestMotion : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftPinterestMotion
+@end
---
+++ Swift/SwiftPinterestMotion/SwiftPinterestMotion/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SwiftPinterestMotion
+//
+//  Created by Takanori.H on 2017/05/24.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    let menuButton = HomeMenuButton(image: UIImage(named:"menu")!, size: CGSize(width: 50, height: 50))
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        
+    }
+
+
+}
+
